24170775d5addabbddca9ab32be0db8587e1582f
ian <ianopolous@protonmail.com>
2022-09-28 21:59:33 +0100

    Basic android app working using a webview and remote server.

--- src/main/java/org/peergos/sample/Main.java
+++ src/main/java/org/peergos/sample/Main.java
@@ -1,77 +1,68 @@
 package org.peergos.sample;
 
-import com.gluonhq.attach.display.DisplayService;
-import com.gluonhq.attach.util.Platform;
-import com.gluonhq.charm.glisten.application.AppManager;
-import com.gluonhq.charm.glisten.control.AppBar;
-import com.gluonhq.charm.glisten.control.FloatingActionButton;
-import com.gluonhq.charm.glisten.mvc.View;
-import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
-import com.gluonhq.charm.glisten.visual.Swatch;
 import javafx.application.Application;
-import javafx.geometry.Dimension2D;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 import static com.gluonhq.charm.glisten.application.AppManager.HOME_VIEW;
 
 public class Main extends Application {
 
-    private final AppManager appManager = AppManager.initialize(this::postInit);
-
-    @Override
-    public void init() {
-        appManager.addViewFactory(HOME_VIEW, () -> {
-            FloatingActionButton fab = new FloatingActionButton(MaterialDesignIcon.SEARCH.text,
-                    e -> System.out.println("Search"));
-
-            ImageView imageView = new ImageView(new Image(Main.class.getResourceAsStream("openduke.png")));
-            imageView.setFitHeight(200);
-            imageView.setPreserveRatio(true);
-
-            Label label = new Label("Hello, Peergos!");
-
-            VBox root = new VBox(20, imageView, label);
-            root.setAlignment(Pos.CENTER);
-
-            View view = new View(root) {
-                @Override
-                protected void updateAppBar(AppBar appBar) {
-                    appBar.setTitleText("Peergos");
-                }
-            };
-
-            fab.showOn(view);
-
-            return view;
-        });
-    }
+    private Scene scene;
 
     @Override
     public void start(Stage stage) {
-        appManager.start(stage);
-    }
-
-    private void postInit(Scene scene) {
-        Swatch.LIGHT_GREEN.assignTo(scene);
-        scene.getStylesheets().add(Main.class.getResource("styles.css").toExternalForm());
-        ((Stage) scene.getWindow()).getIcons().add(new Image(Main.class.getResourceAsStream("/icon.png")));
-
-        if (Platform.isDesktop()) {
-            Dimension2D dimension2D = DisplayService.create()
-                    .map(DisplayService::getDefaultDimensions)
-                    .orElse(new Dimension2D(640, 480));
-            scene.getWindow().setWidth(dimension2D.getWidth());
-            scene.getWindow().setHeight(dimension2D.getHeight());
-        }
+        // create the scene
+        stage.setTitle("Peergos");
+        scene = new Scene(new Browser(), 900, 700, Color.web("#666970"));
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    static class Browser extends Region {
+
+        final WebView browser = new WebView();
+        final WebEngine webEngine = browser.getEngine();
+
+        public Browser() {
+            //apply the styles
+            getStyleClass().add("Peergos");
+            // load the web page
+            webEngine.load("https://peergos.net/");
+            //add the web view to the scene
+            getChildren().add(browser);
+
+        }
+
+        @Override
+        protected void layoutChildren() {
+            double w = getWidth();
+            double h = getHeight();
+            layoutInArea(browser, 0, 0, w, h, 0, HPos.CENTER, VPos.CENTER);
+        }
+
+        @Override
+        protected double computePrefWidth(double height) {
+            return 900;
+        }
+
+        @Override
+        protected double computePrefHeight(double width) {
+            return 600;
+        }
     }
 }
