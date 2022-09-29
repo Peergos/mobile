cfc750376cd9db8457eb0a6ca64fa9e3375169dd
ian <ianopolous@protonmail.com>
2022-09-29 22:58:18 +0100

    Add Peergos.jar, run a localhost server, allow http in webview.
    Try to include web assets - currently failing.

--- src/main/java/org/peergos/sample/Main.java
+++ src/main/java/org/peergos/sample/Main.java
@@ -11,6 +11,16 @@ import javafx.application.Application;
 import javafx.geometry.*;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
+import peergos.server.*;
+import peergos.server.messages.*;
+import peergos.server.storage.*;
+import peergos.shared.*;
+import peergos.shared.io.ipfs.cid.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class Main extends Application {
 
@@ -19,6 +29,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
+        startServer();
+
         // create the scene
         stage.setTitle("Peergos");
         view = new Browser();
@@ -34,6 +46,28 @@ public class Main extends Application {
         stage.show();
     }
 
+    public void startServer() {
+        try {
+            Crypto crypto = Builder.initCrypto();
+            NetworkAccess net = Builder.buildJavaNetworkAccess(new URI("https://peergos.net").toURL(), true).join();
+            ServerMessageStore serverMessages = null; // TODO
+            UserService server = new UserService(net.dhtClient, net.batCave, crypto, net.coreNode, net.account,
+                    net.social, net.mutable, net.instanceAdmin, net.spaceUsage, serverMessages, null);
+
+            InetSocketAddress localAPIAddress = new InetSocketAddress("localhost", 8000);
+            List<String> appSubdomains = Arrays.asList("markdown-viewer,email,calendar,todo-board,code-editor,pdf".split(","));
+            Cid nodeId = net.dhtClient.id().join();
+            int connectionBacklog = 50;
+            int handlerPoolSize = 4;
+            server.initAndStart(localAPIAddress, nodeId, Optional.empty(), Optional.empty(),
+                    Collections.emptyList(), Collections.emptyList(), appSubdomains, true,
+                    Optional.empty(), Optional.empty(), Optional.empty(), true, false,
+                    connectionBacklog, handlerPoolSize);
+        } catch (URISyntaxException | IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -47,7 +81,7 @@ public class Main extends Application {
             //apply the styles
             getStyleClass().add("Peergos");
             // load the web page
-            webEngine.load("https://peergos.net/");
+            webEngine.load("http://localhost:8000/");
             //add the web view to the scene
             getChildren().add(browser);
         }
